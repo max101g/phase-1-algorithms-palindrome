40acbeb9e503b390f2b161a3d9fa7602363190f6
max101g <maxgithinji101@gmail.com>
2022-12-13 10:47:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   const reverseWord = reverseString(word);
+
+   if(word === reverseWord){
+    return true;
+   }else{
+    return false;
+   }
 }
 
+function reverseString(word){
+  const wordArray = word.split("");
+  const reverseWordArray = wordArray.reverse();
+  const reverseWord = reverseWordArray.join("");
+
+  return reverseWord;
+}
 /* 
   Add your pseudocode here
+   If the input string is the same after I reverse it, I should return true
 */
 
 /*
@@ -16,10 +31,21 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("ab"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"))  
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
